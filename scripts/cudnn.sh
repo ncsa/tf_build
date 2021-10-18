6f1f0916f676efba0f63bb70e7f1941479333a46
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-10-18 13:38:47 -0500

    Add cudnn.sh script

---
+++ scripts/cudnn.sh
@@ -0,0 +1,14 @@
+# Prepare cudnn (iforge)
+cudnn_pkgver1=8.2.2
+cudnn_cudaver=11.4
+cudnn_pkgver2=8.2.2.26
+cudnn_filename=cudnn-${cudnn_cudaver}-linux-x64-v${cudnn_pkgver2}.tgz
+if [ ! -e ${cudnn_filename} ]; then
+    # Download Cudnn package
+    wget "https://developer.download.nvidia.com/compute/redist/cudnn/v${cudnn_pkgver1}/${cudnn_filename}"
+fi;
+
+if [ ! -e ${CONDA_PREFIX}/include/cudnn_version.h ]; then
+    # Install cudnn
+    tar xvf ${cudnn_filename} -C ${CONDA_PREFIX} --strip-components 1
+fi;
