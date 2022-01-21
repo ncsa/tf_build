e1ec0c96dacc6bab827eb713cb3afa0a0d02fc08
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2022-01-21 11:15:01 -0600

    Add test_mnist.py script

---
+++ test_mnist.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True)
+
+def normalize_img(image, label):
+    return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+print("Training simple model with dense layers")
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+model.fit(
+    ds_train,
+    epochs=6,
+    validation_data=ds_test,
+)
+
+print("Training simple model with 2d convolutional layers")
+
+model2 = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
+    tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10)
+])
+
+model2.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+model2.fit(
+    ds_train,
+    epochs=6,
+    validation_data=ds_test,
+)
